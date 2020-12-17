b069359cd01cca81f77b89f5a0424b26378ebf71
NikitaJain1110 <56667079+NikitaJain1110@users.noreply.github.com>
2020-12-17 11:02:50 +0530

    getting ready

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const UserRoutes = require("./Routes/user");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 var cors = require("cors");
+const path = require('path')
 
 app.use(morgan("dev"));
 app.use(cors());
@@ -32,9 +33,17 @@ mongoose.connect(
 app.use("/api/restaurant", AdminRoutes);
 app.use("/api/user", UserRoutes);
 
-// app.use('/api/', DishRoutes);
+//Serve ststic assets if in production
+if(process.env.NODE_ENV === "production"){
+  //Set static folder
+  app.use(express.static('client/build'))
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+  })
+}
 
-const PORT = 7000;
+
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
